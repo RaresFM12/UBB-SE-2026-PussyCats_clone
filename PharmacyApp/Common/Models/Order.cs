05b4e685a23f4548734085c8d28a82bd3ec3d3e8
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2026-04-23 16:37:31 +0300

    fixing syntax for order, substance

--- PharmacyApp/Common/Models/Order.cs
+++ PharmacyApp/Common/Models/Order.cs
@@ -8,18 +8,23 @@ namespace PharmacyApp.Models
         public const int OrderExpirationDays = 7;
 
         public int Id { get; private set; }
-        public string IdString { get { return "Order#" + Id;  } }
+        public string IdString
+        {
+            get { return "Order#" + Id;  }
+        }
         public int ClientId { get; set; }
         public DateOnly PickUpDate { get; set; }
-        public string PickUpDateString { get { return PickUpDate.ToString("yyyy.MM.dd"); } }
-        public string ExpirationDateString { get { return PickUpDate.AddDays(OrderExpirationDays).ToString("yyyy.MM.dd"); } }
+        public string PickUpDateString
+        {
+            get { return PickUpDate.ToString("yyyy.MM.dd"); }
+        }
+        public string ExpirationDateString
+        {
+            get { return PickUpDate.AddDays(OrderExpirationDays).ToString("yyyy.MM.dd"); }
+        }
         public bool IsCompleted { get; set; }
         public bool IsExpired { get; set; }
-
-        // the tuple contains for every item its quantity and price
         public Dictionary<int, Tuple<int, float>> ItemQuantitiesWithFinalPrice { get; private set; }
-
-
         public Order(int id, int clientId, DateOnly pickUpDate,
                      bool isCompleted = false, bool isExpired = false)
         {
@@ -33,28 +38,41 @@ namespace PharmacyApp.Models
 
         public bool Equals(Order other)
         {
-            if (other is null) return false;
+            if (other is null)
+            {
+                return false;
+            }
+
             return this.Id == other.Id;
         }
 
         public void AddItemToOrder(int newItemId, int itemQuantity, float finalPrice)
         {
             if (ItemQuantitiesWithFinalPrice.ContainsKey(newItemId))
+            {
                 throw new ArgumentException("Item #" + newItemId + " already exists in order");
+            }
+
             ItemQuantitiesWithFinalPrice[newItemId] = new Tuple<int, float>(itemQuantity, finalPrice);
         }
 
         public void ChangeItemInfoInOrder(int itemId, int newItemQuantity, float newFinalPrice)
         {
-            if (!ItemQuantitiesWithFinalPrice.ContainsKey(itemId)) 
+            if (!ItemQuantitiesWithFinalPrice.ContainsKey(itemId))
+            {
                 throw new ArgumentException("Item #" + itemId + " doesn't exist");
+            }
+
             ItemQuantitiesWithFinalPrice[itemId] = new Tuple<int, float>(newItemQuantity, newFinalPrice);
         }
 
         public void RemoveItemFromOrder(int itemId)
         {
             if (!ItemQuantitiesWithFinalPrice.ContainsKey(itemId))
+            {
                 throw new ArgumentException("Item #" + itemId + " doesn't exist");
+            }
+
             ItemQuantitiesWithFinalPrice.Remove(itemId);
         }
 
